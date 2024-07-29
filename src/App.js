1fe1e8b17492e1669e1c25fa8c75c3632837d38c
nc133878 <crawford.neal18@gmail.com>
2024-07-29 00:57:36 -0400

    Updated eslint errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import Footer from "./components/Footer";
 import Form from "./components/Form";
 import Hero from "./components/Hero";
 import Info from "./components/Info";
-import Tailwind from "./components/Tailwind";
 
 function App() {
   return (
